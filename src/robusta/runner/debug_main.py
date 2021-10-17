de995a39101d46ed7f7b672f83b01e2d30517b37
asaf-horvitz <59296655+asaf-horvitz@users.noreply.github.com>
2021-10-17 06:37:11 +0000

    debug main another block

--- src/robusta/runner/debug_main.py
+++ src/robusta/runner/debug_main.py
@@ -23,7 +23,11 @@ def main():
     markdown = MarkdownBlock ('markdown text')    
     enrichment = Enrichment([markdown])
     finding.enrichments.append()
-    
+
+    markdown = MarkdownBlock ('markdown text 22222')    
+    enrichment = Enrichment([markdown])
+    finding.enrichments.append()
+
     hook_url = "https://robusta650.webhook.office.com/webhookb2/b8b2b92a-02e9-4f5b-9c6f-3b77010d9cc6@34408606-07e6-4a82-98ac-c3668f4e57f5/IncomingWebhook/5479ec3149a34b99a0c7bca141787950/82e528f7-78de-4ded-9b84-0c6eb2c4883a"
     msteams_sink = MsTeamsSink('')
     msteams_sink.write_finding_debug(hook_url, finding)
