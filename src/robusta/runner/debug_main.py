a8b09dbc9748d857b9407d50b403bb50106b835b
asaf-horvitz <59296655+asaf-horvitz@users.noreply.github.com>
2021-10-17 06:45:13 +0000

    debug main add another enrichment

--- src/robusta/runner/debug_main.py
+++ src/robusta/runner/debug_main.py
@@ -4,9 +4,11 @@ import os
 import os.path
 from inspect import getmembers
 
+from src.robusta.core.reporting.blocks import Enrichment
+
 os.chdir('/app/robusta/runner')
 
-from ..core.reporting.blocks import Enrichment, Finding, MarkdownBlock
+from ..core.reporting.blocks import Finding, MarkdownBlock
 from ..core.sinks.msteams.msteams_sink import MsTeamsSink,MsTeamsSinkConfig
 
 
@@ -18,16 +20,17 @@ def main():
 
     finding = Finding('some title')
     finding.title = 'test'
-    finding.description = 'this is a short desc \n\n another line'
+    finding.description = 'this is a short desc\n\\nanother line'
 
-    markdown = MarkdownBlock ('markdown text')    
+    markdown = MarkdownBlock('markdown text<br><br>11111\n2222')    
     enrichment = Enrichment([markdown])
-    finding.enrichments.append()
+    finding.enrichments.append(enrichment)
 
-    markdown = MarkdownBlock ('markdown text 22222')    
+    markdown = MarkdownBlock('markdown text 22222<br><br>11111\n2222')
     enrichment = Enrichment([markdown])
-    finding.enrichments.append()
+    finding.enrichments.append(enrichment)
 
+    
     hook_url = "https://robusta650.webhook.office.com/webhookb2/b8b2b92a-02e9-4f5b-9c6f-3b77010d9cc6@34408606-07e6-4a82-98ac-c3668f4e57f5/IncomingWebhook/5479ec3149a34b99a0c7bca141787950/82e528f7-78de-4ded-9b84-0c6eb2c4883a"
     msteams_sink = MsTeamsSink('')
     msteams_sink.write_finding_debug(hook_url, finding)
