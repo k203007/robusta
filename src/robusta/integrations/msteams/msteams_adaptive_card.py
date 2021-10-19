f29023a169876e9083248ce33f24029817304e26
asaf-horvitz <59296655+asaf-horvitz@users.noreply.github.com>
2021-10-19 12:04:10 +0000

    create logic for adaptive card http

---
+++ src/robusta/integrations/msteams/msteams_adaptive_card.py
@@ -0,0 +1,48 @@
+
+from enum import Enum
+
+
+class AdaptiveCardFontSize(Enum):
+    SMALL = "small"
+    MEDIUM = "medium"
+    LARGE = "large"
+    EXTRA_LARGE = "extraLarge"
+
+
+class MsTeamsAdaptiveCard:
+    FONT_SIZE_SMALL = "small"
+    "medium"
+    "large"
+    "extraLarge"'
+
+    def setBody(self, body : str):
+        __BODY =  """{
+                        "type":"message",
+                        "attachments":[
+                        {
+                            "contentType":"application/vnd.microsoft.card.adaptive",
+                            "contentUrl":null,
+                            "content":{
+                                "$schema":"http://adaptivecards.io/schemas/adaptive-card.json",
+                                "type":"AdaptiveCard",
+                                "version":"1.2",
+                                "body":[
+                                    {0}  
+                                ]
+                            }
+                        }
+                    }"""
+
+        return body.format(body)
+
+
+    def set_text_block(self, text : str, font_size : AdaptiveCardFontSize):
+        block = ''' {
+                "type": "TextBlock",
+                "text": "{0}",
+                "size": "{1}"
+                } '''
+        return block.format(text, font_size.value)
+
+    def set_image(self):
+        pass
